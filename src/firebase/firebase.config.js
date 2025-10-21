93bc93c0d5986537602d1569b09986d01068adce
Obaidul1545 <mdobaidulislam128@gmail.com>
2025-10-21 13:37:30 +0600

    firebase apikey hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAxFIFuVzO6PvI73VDOMFoZ4W54UzNSWxk',
-  authDomain: 'dragon-news-7d828.firebaseapp.com',
-  projectId: 'dragon-news-7d828',
-  storageBucket: 'dragon-news-7d828.firebasestorage.app',
-  messagingSenderId: '703321248169',
-  appId: '1:703321248169:web:e7e751481f33dbe425f4b8',
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
